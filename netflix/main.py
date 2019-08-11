e8f864e41a1374e46d0ebbe709f2827ac91eb7a7
Dawei Wang <daweiw@mun.ca>
2019-08-10 23:52:53 -0230

    Added EM algorithm in naive_em.py

--- netflix/main.py
+++ netflix/main.py
@@ -7,9 +7,11 @@ import em
 X = np.loadtxt("toy_data.txt")
 
 # TODO: Your code here
-K = 4
+K = 3
 seed = 0
 mixture, post = common.init(X, K, seed)
-mixture, post, cost = kmeans.run(X, mixture, post)
-print(cost)
+#mixture, post, cost = kmeans.run(X, mixture, post)
+#print(cost)
+mixture, post, LL = naive_em.run(X, mixture, post)
+print(LL)
 common.plot(X, mixture, post, "K = %d" % K)
--- netflix/naive_em.py
+++ netflix/naive_em.py
@@ -17,8 +17,25 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
-
+    n, d = X.shape
+    K, _ = mixture.mu.shape
+    post = np.zeros((n, K))
+    LL = 0.0
+    
+    for i in range(n):
+        tiled_vector = np.tile(X[i,:], (K,1))
+        # Compute p(x^(i)|k) = N(x^(i); mu_k, sigma2_k) for all k
+        post[i,:] = np.exp(-((tiled_vector - mixture.mu)**2).sum(axis=1) / (2*mixture.var)) / np.power(2 * np.pi * mixture.var, d/2)
+        # Compute p(x^(i)|k)*p(k) for all k
+        post[i,:] = post[i,:] * mixture.p
+        pxi = np.sum(post[i,:])
+        LL += np.log(pxi)
+        post[i,:] /= pxi
+        
+    return post, LL
+        
+    
+    
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     """M-step: Updates the gaussian mixture by maximizing the log-likelihood
@@ -32,8 +49,21 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
-
+    n, K = post.shape
+    _, d = X.shape
+    n_hat = np.sum(post, axis=0)
+    p = n_hat/n
+    
+    mu = np.zeros((K,d))
+    var = np.zeros(K)
+    
+    for k in range(K):
+        mu[k,:] = post[:,k] @ X / post[:,k].sum()
+        var[k] = ((X - mu[k,:])**2).sum(axis=1) @ post[:,k] / (d * post[:,k].sum())
+        
+    return GaussianMixture(mu, var, p)
+    
+    
 
 def run(X: np.ndarray, mixture: GaussianMixture,
         post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
@@ -50,4 +80,12 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    previous_LL = None
+    LL = None
+    while (previous_LL is None or LL - previous_LL > 1e-6 * np.abs(LL)):
+        previous_LL = LL
+        post, LL = estep(X, mixture)
+        print(LL)
+        mixture = mstep(X, post)
+        
+    return mixture, post, LL
